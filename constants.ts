f73bd382c20874c67aa00b10111ac164da27eb51
nahumsvr <nahumnea@gmail.com>
2026-04-19 19:52:42 -0600

    feat: implement authentication middleware and remove default landing page

--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
 export const API_URL = "http://localhost:3100";
+export const TOKEN_NAME = "auth_ocso_token";
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import { TOKEN_NAME } from "./constants";
+
+export default function Midelware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const token = req.cookies.get(TOKEN_NAME)?.value;
+  console.log(path);
+  if (path.startsWith("/dashboard")) {
+    if (!token) return NextResponse.redirect(new URL("/login", req.url));
+  }
+  if (path == "/") {
+    if (token) return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+  return NextResponse.next();
+}
